a73e078c3309f494f23723fa01f732ffd02b5847
Chih-Cheng Liang <chihchengliang@gmail.com>
2017-07-10 22:33:50 +0800

    localization

--- client/src/App.js
+++ client/src/App.js
@@ -5,28 +5,6 @@ import TimeInfo from './components/time-info';
 import Logos from './components/logos';
 
 
-class Nav extends Component {
-  render() {
-    return (
-      <div>
-        <nav className="navbar">
-          <div className="navbar-brand">
-            <a className="navbar-item" href="http://bulma.io">
-              <h1>Taipei Ethereum Meetup</h1>
-            </a>
-
-            <div className="navbar-burger">
-              <span></span>
-              <span></span>
-              <span></span>
-            </div>
-          </div>
-        </nav>
-      </div>
-    )
-  }
-}
-
 class Header extends Component {
   render() {
     return (
@@ -34,13 +12,52 @@ class Header extends Component {
         <div className="hero-body">
           <div className="container">
             <h1 className="title"> 2017 LogoVote</h1>
-            <h2 className="subtitle"> Taipei Ethereum Meetup</h2>
+            <h2 className="subtitle">台北以太坊社群 Taipei Ethereum Meetup</h2>
+            <h3>主合約地址：
+              <a target="_blank"
+                href="https://etherscan.io/address/0x3ab1d534bb477f516817efaaf0b569f419b8e292"
+                rel="noopener noreferrer">
+                0x3Ab1d534Bb477f516817eFaAf0B569f419b8e292</a>
+            </h3>
           </div>
         </div>
       </section>
     )
   }
 }
+
+class HowTo extends Component {
+  render() {
+    return (
+      <section className="hero is-medium is-bold">
+        <div className="hero-body">
+          <div className="container">
+            <h1 className="title"> How do I vote?</h1>
+            <h2 className="subtitle"> Let me tell ya</h2>
+
+          </div>
+        </div>
+      </section>
+    )
+  }
+}
+
+class LearnMore extends Component {
+  render() {
+    return (
+      <section className="hero is-medium is-bold">
+        <div className="hero-body">
+          <div className="container">
+            <h1 className="title"> How do I vote?</h1>
+            <h2 className="subtitle"> Let me tell ya</h2>
+
+          </div>
+        </div>
+      </section>
+    )
+  }
+}
+
 
 class App extends Component {
   render() {
@@ -49,6 +66,8 @@ class App extends Component {
         <Header />
         <TimeInfo />
         <Logos />
+        <HowTo />
+        <LearnMore />
       </div>
     );
   }
